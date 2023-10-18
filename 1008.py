31a20e0b2f92382e4989960f2b7c900166a5a348
Júlia Silvestre <juliasilvestreribeiro2004@gmail.com>
2023-10-18 19:14:42 -0300

    problema 1008 resolvido

---
+++ 1008.py
@@ -0,0 +1,6 @@
+numero_de_funcionario = int(input(""))
+horas_trabalhadas = int(input(""))
+salario = float(input(""))
+print("NUMBER =",numero_de_funcionario)
+salario = horas_trabalhadas * salario
+print(f"SALARY = U$ {salario:.2f}")
