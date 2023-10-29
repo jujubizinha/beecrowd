c134e0dc4d225d0e6e8c8b853da1fab2fb57660d
Júlia Silvestre <juliasilvestreribeiro2004@gmail.com>
2023-10-28 23:35:35 -0300

    problema 1049 resolvido

---
+++ 1049.py
@@ -0,0 +1,20 @@
+animal1 = str(input())
+animal2 = str(input())
+animal3 = str(input())
+if animal1 == "vertebrado" and animal2 == "ave" and animal3 == "carnivoro":
+    print("aguia")
+if animal1 == "vertebrado" and animal2 == "ave" and animal3 == "onivoro":
+    print("pomba")
+if animal1 == "vertebrado" and animal2 == "mamifero" and animal3 == "onivoro":
+        print("homem")
+if animal1 == "vertebrado" and animal2 == "mamifero" and animal3 == "herbivoro":
+            print("vaca")
+if animal1 == "invertebrado" and animal2 == "inseto" and animal3 == "hematofago":
+        print("pulga")
+if animal1 == "invertebrado" and animal2 == "inseto" and animal3 == "herbivoro":
+        print("lagarta")
+if animal1 == "invertebrado" and animal2 == "anelideo" and animal3 == "hematofago":
+        print("sanguessuga")
+if animal1 == "invertebrado" and animal2 == "anelideo" and animal3 == "onivoro":
+        print("minhoca")
+
