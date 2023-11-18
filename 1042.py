6496cc6e3ae00baef541e911f5ce03b4e22c2a97
Júlia Silvestre <juliasilvestreribeiro2004@gmail.com>
2023-11-18 19:48:02 -0300

    problema 1042 resolvido

---
+++ 1042.py
@@ -0,0 +1,10 @@
+numero1, numero2, numero3 = map(int,input().split())
+numeros = [numero1, numero2, numero3]
+numeros.sort()
+print(numeros[0])
+print(numeros[1])
+print(numeros[2])
+print("")
+print(numero1)
+print(numero2)
+print(numero3)
